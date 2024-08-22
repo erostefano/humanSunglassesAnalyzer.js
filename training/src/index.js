acc9ad67b82edd9dc921602f9d111edb2ba7bfc8
Stefano Mavilio <st.mavilio@gmail.com>
2024-08-22 09:40:07 +0200

    fix(training): use async/await

--- training/src/index.js
+++ training/src/index.js
@@ -1,5 +1,4 @@
 // TODO: create a confusion matrix using .predict and the test data
-// TODO: use async
 // TODO: creating multiple models using different configs
 
 const tf = require('@tensorflow/tfjs-node');
@@ -10,31 +9,24 @@ logger.info('Start training');
 
 const {xTrain, yTrain, xTest, yTest} = require("./data");
 
-cnn.fit(xTrain, yTrain, {
-    epochs: 10,             // Number of epochs
-    batchSize: 32,          // Number of samples per gradient update
-    callbacks: tf.callbacks.earlyStopping({patience: 3}) // Optional: stops training early if no improvement
-})
-    .then(history => {
-        logger.info('Training completed');
-        logger.info('History', JSON.stringify(history));
-        logger.info('Final training accuracy', history.history.acc);
-        logger.info('Final validation accuracy', history.history.val_acc);
-
-        return cnn.evaluate(xTest, yTest);
-    })
-    .then(testResult => {
-        logger.info('Testresult', JSON.stringify(testResult))
-        const [lossTensor, accuracyTensor] = testResult;
-
-        logger.info('Test Loss', lossTensor.dataSync());
-        logger.info('Test Accuracy', accuracyTensor.dataSync());
-
-        return cnn.save('file://model');
-    })
-    .then(() => {
-        logger.info('Model saved to disk');
-    })
-    .catch(error => {
-        logger.error('Error during training, evaluation, or saving', error);
+(async () => {
+    const history = await cnn.fit(xTrain, yTrain, {
+        epochs: 10,             // Number of epochs
+        batchSize: 32,          // Number of samples per gradient update
+        callbacks: tf.callbacks.earlyStopping({patience: 3}) // Optional: stops training early if no improvement
     });
+
+    logger.info('Training completed');
+    logger.info('History', JSON.stringify(history));
+    logger.info('Final training accuracy', history.history.acc);
+    logger.info('Final validation accuracy', history.history.val_acc);
+
+    const testResult = cnn.evaluate(xTest, yTest);
+    logger.info('Testresult', JSON.stringify(testResult))
+
+    const [lossTensor, accuracyTensor] = testResult;
+    logger.info('Test Loss', lossTensor.dataSync());
+    logger.info('Test Accuracy', accuracyTensor.dataSync());
+
+    await cnn.save('file://model');
+})();
