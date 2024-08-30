53045e35ea96fd84ac18f8d5e7d80937766a4e34
Stefano Mavilio <st.mavilio@gmail.com>
2024-08-30 18:03:17 +0200

    feat(feature-engineering): shuffle and rename

--- feature-engineering/rename-and-random-sort.js
+++ feature-engineering/rename-and-random-sort.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // Replace with your folder path
-const folderPath = 'your-folder-path';
+const folderPath = 'with-sunglasses';
 
 // Function to randomly shuffle an array
 function shuffle(array) {
@@ -10,19 +10,6 @@ function shuffle(array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
-}
-
-// Function to generate a unique file name to avoid overwriting
-function generateUniqueName(folderPath, baseName, extension) {
-    let name = `${baseName}${extension}`;
-    let counter = 1;
-
-    while (fs.existsSync(path.join(folderPath, name))) {
-        name = `${baseName}-${counter}${extension}`;
-        counter++;
-    }
-
-    return name;
 }
 
 fs.readdir(folderPath, (err, files) => {
@@ -37,22 +24,24 @@ fs.readdir(folderPath, (err, files) => {
     // Filter only files (exclude folders)
     files = files.filter(file => fs.lstatSync(path.join(folderPath, file)).isFile());
 
-    // Rename files
+    // First pass: Rename to temporary unique names
     files.forEach((file, index) => {
-        const baseName = `with-sunglasses-${index + 1}`;
-        const extension = path.extname(file);
+        const tempFileName = `tempfile-${index}${path.extname(file)}`;
         const oldPath = path.join(folderPath, file);
+        const tempPath = path.join(folderPath, tempFileName);
 
-        // Generate a unique new file name
-        const newFileName = generateUniqueName(folderPath, baseName, extension);
-        const newPath = path.join(folderPath, newFileName);
-
-        fs.rename(oldPath, newPath, (err) => {
-            if (err) {
-                console.error(`Error renaming file ${file}:`, err);
-            } else {
-                console.log(`${file} renamed to ${newFileName}`);
-            }
-        });
+        fs.renameSync(oldPath, tempPath);
     });
+
+    // Second pass: Rename from temporary names to final names
+    files.forEach((file, index) => {
+        const tempFileName = `tempfile-${index}${path.extname(file)}`;
+        const finalFileName = `with-sunglasses-${index + 1}${path.extname(file)}`;
+        const tempPath = path.join(folderPath, tempFileName);
+        const finalPath = path.join(folderPath, finalFileName);
+
+        fs.renameSync(tempPath, finalPath);
+    });
+
+    console.log("Files have been renamed successfully without any loss.");
 });
